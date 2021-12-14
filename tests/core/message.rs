ef3b8edf6e550f138762ed86f927e98621fd8169
Thiago Vicente <thiagovice@gmail.com>
2021-12-13 22:25:29 -0500

    Add test for content-length parsing

--- tests/core/message.rs
+++ tests/core/message.rs
@@ -1,6 +1,7 @@
 use libsip::*;
 
 use nom::error::VerboseError;
+use libsip::Header::ContentLength;
 
 #[test]
 fn read_message() {
@@ -15,6 +16,24 @@ fn read_message() {
         Ok((remains.as_ref(), req)),
         parse_message::<VerboseError<&[u8]>>(b"REGISTER sip:example.com SIP/2.0\r\n\r\n")
     );
+}
+
+#[test]
+fn parse_content_length() {
+    let raw = concat!(
+            "INVITE sip:14074560998@67.231.1.154:5060 SIP/2.0\r\n",
+            "Via: SIP/2.0/UDP 64.95.59.218:5092;branch=z9hG4bKESg0gXKq4g9Ftph3C35EF7\r\n",
+            "Secret-Letter: SuperSecretHeader\r\n",
+            "Content-Type: application/sdp\r\n",
+            "Content-Length: 128\r\n\r\n").as_bytes();
+    let msg = parse_message::<VerboseError<&[u8]>>(raw);
+    assert!(msg.is_ok());
+    assert!(msg.clone().unwrap().1.headers().content_length().is_some());
+    let cl = match msg.unwrap().1.headers().content_length() {
+        Some(ContentLength(a)) => a,
+        _ => 0,
+    };
+    assert_eq!(cl, 128);
 }
 
 #[test]
